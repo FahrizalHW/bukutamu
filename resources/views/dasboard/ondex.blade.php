916c135562a3a5bd5d49ec7d2795fc49955b2aaf
Nabil-Kaindra <nabilkaindrabudiantana@gmail.com>
2024-09-09 15:35:22 +0700

    gg

--- resources/views/dasboard/ondex.blade.php
+++ resources/views/dasboard/ondex.blade.php
@@ -58,6 +58,25 @@
   </div>
 </div>
 
+<script>
+    function confirmDelete(event) {
+        event.preventDefault(); // Stop the form from submitting immediately
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                event.target.submit(); // Submit the form if confirmed
+            }
+        })
+    }
+</script>
 
 @endsection
 
