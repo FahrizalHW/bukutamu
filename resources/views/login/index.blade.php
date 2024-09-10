05692c6e42bf651e58679b1364bda4e5d063c376
Nabil-Kaindra <nabilkaindrabudiantana@gmail.com>
2024-09-10 14:05:56 +0700

    panas 1 dan mcnugget 12pc

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,61 +10,112 @@
   <link rel="stylesheet" href="https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/docs-app/css/dist/mdb5/standard/core.min.css">
   <link rel="stylesheet" href="../login-assets/css/style.css">
   <link rel="icon" href="../login-assets/img/logoGuest.ico">
+
+  <style>
+    body {
+      background-color: #f0f0f0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+
+    .login-container {
+      background-color: white;
+      width: 100%;
+      max-width: 400px;
+      border-radius: 10px;
+      box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+      padding: 30px;
+      text-align: center;
+    }
+
+    .logo-container img {
+      width: 150px;
+      height: auto;
+      margin-bottom: 20px;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    .form-control {
+      width: 100%;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ddd;
+    }
+
+    .btn-submit {
+      width: 100%;
+      padding: 10px;
+      background-color: #f55a42;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    .btn-submit:hover {
+      background-color: #f23a22;
+    }
+
+    .back-link {
+      display: block;
+      margin-top: 15px;
+      text-decoration: none;
+      color: #333;
+    }
+  </style>
+
 </head>
 
 <body>
-  <div class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-12 col-sm-10 col-md-12 col-lg-11 col-xl-10">
-        <div class="card d-flex mx-auto my-5">
-          <div class="row">
-            <div class="col-md-4 col-sm-12 col-xs-12 c1 pt-5 pb-5 ps-7 d-flex justify-content-center"">
-              <div class="row mb-5 m-3">  </div> <img src="../login-assets/img/LogoXhool.png" width="250vw" height="300vh" class="mx-auto " alt="Teacher">
-              
-              @if(session()->has('success'))
-				<div class="alert alert-success alert-dismissible fade show" role="alert">
-					{{ session('success') }}
-					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-				</div>
-				@endif
-              <div class="row justify-content-center">
-              </div>
-            </div>
-            <div class="col-md-7 col-sm-12 col-xs-12 c2 px-5">
-
-				@if(session()->has('LoginEror'))
-				<div class="alert alert-danger alert-dismissible fade show" role="alert">
-					{{ session('LoginEror') }}
-					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-				</div>
-				@endif
-              <form action="/login" onsubmit="" method="post" class="px-5 pb-5">
-              @csrf
-                <div class="d-flex">
-                 
-                  <h3 class="font-weight-bold">Log in</h3>
-                </div> 
-           
-        @csrf
-                <input type="text" placeholder="Nama" class="@error('nama')is-invalid @enderror" placeholder="nama@example" id="nama" name="nama" autofocus required value="{{ old('nama') }}">
-                @error('nama')
-                <div class="invalid-feedback">
-							{{ $message }}
-						</div>
-            @enderror
-                <input type="password" name="password" placeholder="Password" required>
-                <button type="submit" class="text-white text-weight-bold bt">Submit</button>
-
-                <a href="/" class="d-block text-center mt-3">&laquo; Back to Home</a>
-
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="login-container">
+    <div class="logo-container">
+      <img src="../login-assets/img/LogoXhool.png" alt="Logo">
     </div>
+
+    @if(session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    <form action="/login" method="post">
+      @csrf
+      <h3 class="font-weight-bold mb-4">Log in</h3>
+
+      @if(session()->has('LoginEror'))
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('LoginEror') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      @endif
+
+      <div class="form-group">
+        <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" placeholder="Nama" required autofocus value="{{ old('nama') }}">
+        @error('nama')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+
+      <div class="form-group">
+        <input type="password" class="form-control" name="password" placeholder="Password" required>
+      </div>
+
+      <button type="submit" class="btn-submit">Submit</button>
+
+      <a href="/" class="back-link">&laquo; Back to Home</a>
+    </form>
   </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </html>
