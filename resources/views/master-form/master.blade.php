5aaf77cb096fb69c677b764b937285b4b9ecf578
FahrizalHW <hutaothebest666@gmail.com>
2024-10-01 21:35:07 +0700

    sedikit responsive

--- resources/views/master-form/master.blade.php
+++ resources/views/master-form/master.blade.php
@@ -14,6 +14,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jdatepicker@1.11.5/dist/compiled/datepicker.min.css">
     
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
     
     <title>Buku Tamu</title>
 </head>
